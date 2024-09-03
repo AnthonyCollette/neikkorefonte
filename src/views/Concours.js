92d4700b891e507aa43cb3b0a0bcbf50666f4713
Anthony Collette <100pour100inde@gmail.com>
2024-09-03 19:53:22 +0200

    fix giveaways bug

--- src/views/Concours.js
+++ src/views/Concours.js
@@ -4,7 +4,15 @@ import GiveawayResume from '../components/GiveawayResume'
 
 const Concours = () => {
 
-    const [giveaways, setGiveaways] = useState()
+    const [giveaways, setGiveaways] = useState(null)
+
+    const getGiveaways = () => {
+        if (giveaways !== null && giveaways[0]) {
+            return giveaways.map((giveaway, index) => <GiveawayResume key={index} id={giveaway?.id} title={giveaway?.content?.title} image={giveaway?.content?.image} richtext={giveaway?.content?.description} finish={giveaway?.content?.finish}  winner={giveaway?.content?.winner} date={giveaway?.content?.Date} />)
+        } else {
+            return <p className="text--default">Il n'y a pas de concours.</p>
+        }
+    }
 
     useEffect(() => {
         fetch('https://api.storyblok.com/v2/cdn/stories?starts_with=giveaways&token=' + process.env.REACT_APP_STORYBLOK_TOKEN).then(res => res.json()).then(data => {
@@ -12,17 +20,13 @@ const Concours = () => {
         })
     }, [])
 
-    useEffect(() => {
-        console.log(giveaways)
-    }, [giveaways])
-
     return (
         <div className='page'>
             <Nav />
             <section className='container giveaways'>
                 <h1>Giveaways</h1>
                 <div className="giveaways-list">
-                    {giveaways? giveaways.map((giveaway, index) => <GiveawayResume key={index} id={giveaway?.id} title={giveaway?.content?.title} image={giveaway?.content?.image} richtext={giveaway?.content?.description} finish={giveaway?.content?.finish}  winner={giveaway?.content?.winner} date={giveaway?.content?.Date} />) : <p class="text--default">Il n'y a pas de concours.</p>}
+                    {getGiveaways()}
                 </div>
             </section>
         </div>
